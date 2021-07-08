221a962b1721619faf123110ff01f9602677698d
Rayaninha <salesrayane11@gmail.com>
2021-07-08 19:46:46 -0300

    tabuleiro e peças

--- src/Tabuleiro.svelte
+++ src/Tabuleiro.svelte
@@ -8,84 +8,107 @@
 	<br>
 	<div class="container1">
 		
-		<table border="2">
-			<tr
-				><td width="50" height="50" bgcolor="white" /><td
-					width="50"
-					height="50"
-					bgcolor="black"
-				/>
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-			</tr><tr
-				><td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-			</tr><tr
-				><td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-			</tr><tr
-				><td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-			</tr><tr
-				><td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-			</tr><tr
-				><td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-			</tr><tr
-				><td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-			</tr><tr
-				><td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-				<td width="50" height="50" bgcolor="black" />
-				<td width="50" height="50" bgcolor="white" />
-			</tr>
-		</table>
+		<body><div class="peça1"></div>
+<body><div class="peça2"></div>
+<body><div class="peça3"></div>
+<body><div class="peça4"></div>
+<body><div class="peça5"></div>
+<body><div class="peça6"></div>
+<body><div class="peça7"></div>
+<body><div class="peça8"></div>
+<body><div class="peça9"></div>
+<body><div class="peça10"></div>
+<body><div class="peça11"></div>
+<body><div class="peça12"></div>
+<body><div class="peça13"></div>
+<body><div class="peça14"></div>
+<body><div class="peça15"></div>
+<body><div class="peça16"></div>
+<body><div class="peça17"></div>
+<body><div class="peça18"></div>
+<body><div class="peça19"></div>
+<body><div class="peça20"></div>
+<body><div class="peça21"></div>
+<body><div class="peça22"></div>
+<body><div class="peça23"></div>
+<body><div class="peça24"></div>
+</body>
+
+<table border="2">
+<tr><td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+      <td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+<td width="50" height="50" bgcolor="black"></td>
+
+<tr><td width="50" height="50"bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+      <td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+<td width="50" height="50" bgcolor="white"></td>
+
+<tr><td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+      <td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+<td width="50" height="50" bgcolor="black"></td>
+
+<tr><td width="50" height="50"bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+      <td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+<td width="50" height="50" bgcolor="white"></td>
+
+<tr><td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+      <td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50"bgcolor="white" ></td>
+<td width="50" height="50" bgcolor="black"></td>
+
+<tr><td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+      <td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+<td width="50" height="50" bgcolor="white"></td>
+
+<tr><td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+      <td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+<td width="50" height="50" bgcolor="black"></td>
+
+<tr><td width="50" height="50"bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+      <td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+		<td width="50" height="50" bgcolor="white"></td>
+		<td width="50" height="50" bgcolor="black"></td>
+<td width="50" height="50" bgcolor="white"></td>
+
+
+   </table>
 	</div>
 </div>
 
@@ -119,4 +142,284 @@
 		background: white;
 		color: rgb(31, 54, 54);
 	}
+	body{
+	font-family: arial, helvertica, sans-serif;
+   	background-image: linear-gradient(200deg, violet, purple);
+   	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%,-50%);
+}
+
+/*Pessas Marrom*/
+/*Primeira Fileira*/
+.peça1{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 1px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 0%;
+	left: 13.5%;
+}
+
+.peça2{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 0%;
+	left: 38%;
+}
+
+.peça3{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 0%;
+	left: 63%;
+}
+
+.peça4{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 0%;
+	left: 87.5%;
+}
+
+/*Segunda Fileira*/
+.peça5{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 13%;
+	left: 1%;
+}
+
+.peça6{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 13%;
+	left: 26%;
+}
+
+.peça7{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 13%;
+	left: 50.5%;
+}
+
+.peça8{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 13%;
+	left: 75.5%;
+}
+
+/*Terceira Fileira*/
+.peça9{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 25%;
+	left: 13.5%;
+}
+
+.peça10{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 25%;
+	left: 38.5%;
+}
+
+.peça11{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 25%;
+	left: 63%;
+}
+
+.peça12{
+	width: 50px;
+    height: 50px;
+    background: #964b00;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 25%;
+	left: 87.5%;
+}
+
+/*Peças Brancas*/
+/*Sexta Fileira*/
+.peça13{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 63%;
+	left: 1%;
+}
+
+.peça14{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 63%;
+	left: 26%;
+}
+
+.peça15{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 63%;
+	left: 50.5%;
+}
+
+.peça16{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 63%;
+	left: 75.5%;
+}
+
+/*Setima Fileira*/
+.peça17{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 75%;
+	left: 13.5%;
+}
+
+.peça18{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 75%;
+	left: 38.5%;
+}
+
+.peça19{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 75%;
+	left: 63%;
+}
+
+.peça20{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 75%;
+	left: 87.5%;
+}
+
+/*Oitava Fileira*/
+.peça21{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 87.5%;
+	left: 1%;
+}
+
+.peça22{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 87%;
+	left: 26%;
+}
+
+.peça23{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 87%;
+	left: 50.5%;
+}
+
+.peça24{
+	width: 50px;
+    height: 50px;
+    background: #ffffff;
+    border: 0.5px solid #000000;
+    border-radius: 50%;
+    position: absolute;
+	top: 87%;
+	left: 75.5%;
+}
 </style>
