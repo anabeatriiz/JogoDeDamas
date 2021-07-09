fbccaccaf3de6c7d8c92be68abe9c7547a27b5e4
Felinto <47856850+LucasFelinto@users.noreply.github.com>
2021-07-09 09:28:50 -0300

    Alterando CSS do tabuleiro

--- src/Tabuleiro.svelte
+++ src/Tabuleiro.svelte
@@ -1,425 +1,231 @@
 <div class="container">
+  
+  <div class="navbar">
+    <h2><a href="#/"><i class="fa fa-home" aria-hidden="true" /></a></h2>
+    <h2><a href="#/"><i class="fa fa-play" aria-hidden="true" /></a></h2>
+  </div>
 
-	<div class="navbar">
-		<h2><a href="#/"><i class="fa fa-home" aria-hidden="true" /></a></h2>
-		<h2><a href="#/"><i class="fa fa-play" aria-hidden="true" /></a></h2>
-	</div>
-	<br>
-	<br>
-	<div class="container1">
-		
-		<body><div class="peça1"></div>
-<body><div class="peça2"></div>
-<body><div class="peça3"></div>
-<body><div class="peça4"></div>
-<body><div class="peça5"></div>
-<body><div class="peça6"></div>
-<body><div class="peça7"></div>
-<body><div class="peça8"></div>
-<body><div class="peça9"></div>
-<body><div class="peça10"></div>
-<body><div class="peça11"></div>
-<body><div class="peça12"></div>
-<body><div class="peça13"></div>
-<body><div class="peça14"></div>
-<body><div class="peça15"></div>
-<body><div class="peça16"></div>
-<body><div class="peça17"></div>
-<body><div class="peça18"></div>
-<body><div class="peça19"></div>
-<body><div class="peça20"></div>
-<body><div class="peça21"></div>
-<body><div class="peça22"></div>
-<body><div class="peça23"></div>
-<body><div class="peça24"></div>
-</body>
+  <div class="sobre">
+    <div class="folhaderosto">
+      <h1>Projeto IF - Grupo Dama</h1>
+      <p>
+        Trabalho de Desenvolvimento Web solicitado pelos professores Allan Lima
+        e Ranieri Valença, com o objetivo de ...
+      </p>
+  
+    </div>
 
-<table border="2">
-<tr><td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-      <td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-<td width="50" height="50" bgcolor="black"></td>
+    <div class="cartaozinho">
+      <p><img src="images/rayane.jpeg" /></p>
+      <div class="texto">
+        <h3>
+          <i class="fa fa-address-book" aria-hidden="true" /> Rayane Sales - TSI
+          <br />
+          17 anos
+        </h3>
+        <br>
+        <p>
+          <i class="fa fa-github" aria-hidden="true" /> Rayaninha
+          <br>
+          <i class="fa fa-linkedin-square" aria-hidden="true" /> Rayane Sales
+          <br>
+          <i class="fa fa-linux" aria-hidden="true" /> Usuário Linux
+        </p>
+      </div>
+    </div>
+    <div class="cartaozinho">
+      <p><img src="images/joanna.jpeg" /></p>
+      <div class="texto">
+        <h3>
+          <i class="fa fa-address-book" aria-hidden="true" /> Joanna Victória -
+          TSI
+          <br />
+          16 anos
+        </h3>
+        <br>
+        <p>
+          <i class="fa fa-github" aria-hidden="true" /> Joanna-Victoria
+          <br>
+          <i class="fa fa-linkedin-square" aria-hidden="true" /> Joanna Victória
+          <br>
+          <i class="fa fa-windows" aria-hidden="true" /> Usuário Linux
+        </p>
+      </div>
+    </div>
+    <div class="cartaozinho">
+      <p><img src="images/avatar.jpeg"/></p>
+      <div class="texto">
+        <h3>
+          <i class="fa fa-address-book" aria-hidden="true" /> Lucas Alves - IPI
+          <br />
+          19 anos
+        </h3>
+        <br>
+        <p>
+          <i class="fa fa-github" aria-hidden="true" /> github
+          <br>
+          <i class="fa fa-linkedin-square" aria-hidden="true" /> linkedin
+          <br>
+          <i class="fa fa-windows" aria-hidden="true" /> ---
+        </p>
+      </div>
+    </div>
+    <div class="cartaozinho">
+      <p><img src="images/avatar.jpeg"/></p>
+      <div class="texto">
+        <h3>
+          <i class="fa fa-address-book" aria-hidden="true" /> Lucas Felinto -
+          TSI
+          <br />
+          21 anos
+        </h3>
+        <br>
+        <p>
+          <i class="fa fa-github" aria-hidden="true" /> LucasFelinto
+          <br>
+          <i class="fa fa-linkedin-square" aria-hidden="true" /> linkedin
+          <br>
+          <i class="fa fa-windows" aria-hidden="true" /> ---
+        </p>
+      </div>
+    </div>
+    <div class="cartaozinho">
+      <p><img src="images/paloma.jpeg"/></p>
+      <div class="texto">
+        <h3>
+          <i class="fa fa-address-book" aria-hidden="true" /> Paloma Luiza - IPI
+          <br />
+          23 anos
+        </h3>
+        <br>
+        <p>
+          <i class="fa fa-github" aria-hidden="true" /> github PLuiza12
+          <br>
+          <i class="fa fa-linkedin-square" aria-hidden="true" /> linkedin
+          <br>
+          <i class="fa fa-windows" aria-hidden="true" /> ---
+        </p>
+      </div>
+    </div>
+    <div class="cartaozinho">
+      <p><img src="images/avatar.jpeg"/></p>
+      <div class="texto">
+        <h3>
+          <i class="fa fa-address-book" aria-hidden="true" /> Mizael Julio - TSI
+          <br />
+          17 anos
+        </h3>
+        <br>
+        <p>
+          <i class="fa fa-github" aria-hidden="true" /> github
+          <br>
+          <i class="fa fa-linkedin-square" aria-hidden="true" /> linkedin
+          <br>
+          <i class="fa fa-windows" aria-hidden="true" /> ---
+        </p>
+      </div>
+    </div>
+    <div class="cartaozinho">
+      <p><img src="images/beatriz.jpg"/></p>
+      <div class="texto">
+        <h3>
+          <i class="fa fa-address-book" aria-hidden="true" /> Ana Beatriz - TSI
+          <br />
+          20 anos
+        </h3>
+        <br>
+        <p>
+          <i class="fa fa-github" aria-hidden="true" /> anabeatriiz
+          <br>
+          <i class="fa fa-linkedin-square" aria-hidden="true" /> ana-beatriiz
+          <br>
+          <i class="fa fa-windows" aria-hidden="true" /> Usuário Windows
+        </p>
+      </div>
+    </div>
+  </div>
 
-<tr><td width="50" height="50"bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-      <td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-<td width="50" height="50" bgcolor="white"></td>
 
-<tr><td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-      <td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-<td width="50" height="50" bgcolor="black"></td>
-
-<tr><td width="50" height="50"bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-      <td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-<td width="50" height="50" bgcolor="white"></td>
-
-<tr><td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-      <td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50"bgcolor="white" ></td>
-<td width="50" height="50" bgcolor="black"></td>
-
-<tr><td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-      <td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-<td width="50" height="50" bgcolor="white"></td>
-
-<tr><td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-      <td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-<td width="50" height="50" bgcolor="black"></td>
-
-<tr><td width="50" height="50"bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-      <td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-		<td width="50" height="50" bgcolor="white"></td>
-		<td width="50" height="50" bgcolor="black"></td>
-<td width="50" height="50" bgcolor="white"></td>
-
-
-   </table>
-	</div>
 </div>
+<!--<i class="fa fa-github" aria-hidden="true"></i>-->
+<!--<i class="fa fa-html5" aria-hidden="true"></i>-->
+<!--<i class="fa fa-instagram" aria-hidden="true"></i>-->
+<!--<i class="fa fa-linux" aria-hidden="true"></i>-->
+<!--<i class="fa fa-linkedin-square" aria-hidden="true"></i>-->
+<!--<i class="fa fa-windows" aria-hidden="true"></i>-->
+<!--<i class="fa fa-user-o" aria-hidden="true"></i>-->
+<!--<i class="fa fa-css3" aria-hidden="true"></i>-->
 
+<!--<i class="fa fa-address-book" aria-hidden="true"></i>-->
 <style>
-	.container {
-		font-family: arial, helvertica, sans-serif;
-		background-image: linear-gradient(100deg, purple, violet);
-		width: 100%;
-		height: 100vh;
-	}
-	.container1 {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	.navbar {
-		overflow: hidden;
-		background-color: blueviolet;
-		width: 50%;
-		box-shadow: 4px 5px black;
-	}
-	.navbar a {
-		float: left;
-		display: block;
-		color: white;
-		text-align: center;
-		padding: 14px 16px;
-		text-decoration: none;
-	}
-	.navbar a:hover {
-		background: white;
-		color: rgb(31, 54, 54);
-	}
-	body{
-	font-family: arial, helvertica, sans-serif;
-   	background-image: linear-gradient(200deg, violet, purple);
-   	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%,-50%);
-}
-
-/*Pessas Marrom*/
-/*Primeira Fileira*/
-.peça1{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 1px solid #000000;
-    border-radius: 50%;
+  .container {
+    background-image: url("/images/Textura.png");
+    /* background-color: red; */
+    background-attachment: fixed;
+    background-position: center;
+    background-size: 100vw;
     position: absolute;
-	top: 0%;
-	left: 13.5%;
-}
+  }
 
-.peça2{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 0%;
-	left: 38%;
+.navbar {
+  overflow: hidden;
+  background-color: blueviolet;
+  width: 50%;
+  box-shadow: 4px 5px black;;
 }
-
-.peça3{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 0%;
-	left: 63%;
+.navbar a {
+  float: left;
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
 }
-
-.peça4{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 0%;
-	left: 87.5%;
+.navbar a:hover {
+  background: white;
+  color: rgb(31, 54, 54);;
 }
-
-/*Segunda Fileira*/
-.peça5{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 13%;
-	left: 1%;
+.folhaderosto {
+  padding: 0px 10px 0px 10px;
+  background-color: blueviolet;
+  margin-bottom: 50px;
+  transform: skewY(2deg);
+  text-align: justify;
 }
-
-.peça6{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 13%;
-	left: 26%;
+.folhaderosto h1 {
+  color: white;
+  text-decoration: overline;
+  transform: skewY(-2deg);
+  padding-top: 20px;
 }
-
-.peça7{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 13%;
-	left: 50.5%;
+.folhaderosto p {
+  color: white;
+  transform: skewY(-2deg);
+  padding-bottom: 30px;
 }
-
-.peça8{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 13%;
-	left: 75.5%;
+.sobre {
+  margin-top: 100px;
+  width: 100%;
 }
-
-/*Terceira Fileira*/
-.peça9{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 25%;
-	left: 13.5%;
+.cartaozinho {
+  float: left;
+  width: 300px;
+  height: 400px;
+  background-color: blueviolet;
+  margin-left: 10px;
+  margin-top: 10px;
+  text-align: center;
 }
-
-.peça10{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 25%;
-	left: 38.5%;
+.cartaozinho img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  box-shadow: 4px 5px black;
+  margin-top: 15px;
 }
-
-.peça11{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 25%;
-	left: 63%;
-}
-
-.peça12{
-	width: 50px;
-    height: 50px;
-    background: #964b00;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 25%;
-	left: 87.5%;
-}
-
-/*Peças Brancas*/
-/*Sexta Fileira*/
-.peça13{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 63%;
-	left: 1%;
-}
-
-.peça14{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 63%;
-	left: 26%;
-}
-
-.peça15{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 63%;
-	left: 50.5%;
-}
-
-.peça16{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 63%;
-	left: 75.5%;
-}
-
-/*Setima Fileira*/
-.peça17{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 75%;
-	left: 13.5%;
-}
-
-.peça18{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 75%;
-	left: 38.5%;
-}
-
-.peça19{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 75%;
-	left: 63%;
-}
-
-.peça20{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 75%;
-	left: 87.5%;
-}
-
-/*Oitava Fileira*/
-.peça21{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 87.5%;
-	left: 1%;
-}
-
-.peça22{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 87%;
-	left: 26%;
-}
-
-.peça23{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 87%;
-	left: 50.5%;
-}
-
-.peça24{
-	width: 50px;
-    height: 50px;
-    background: #ffffff;
-    border: 0.5px solid #000000;
-    border-radius: 50%;
-    position: absolute;
-	top: 87%;
-	left: 75.5%;
+.texto {
+  padding: 15px 15px 0px 15px;
+  text-align: justify;
+  color: white;
 }
 </style>
